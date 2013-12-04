029246ce9189f1cdd1fe30a4f4c9996881d20b9e
jcamenen <jcamenen@gmail.com>
2013-12-04 08:16:50 -0800

    Ajout FourmizWorker

---
+++ src/fourmiz/abillity/FourmizWorker.java
@@ -0,0 +1,48 @@
+package fourmiz.abillity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import fourmiz.collision.Entity;
+import fourmiz.collision.TouchHandle;
+import fourmiz.collision.TouchMarker;
+import fourmiz.engine.Abillity;
+
+public class FourmizWorker extends Abillity
+{
+	public boolean buzy = false;
+	
+	public FourmizWorker(Entity owner) 
+	{
+		super(owner);
+	}
+	
+	public boolean isBusy()
+	{
+		return buzy;
+	}
+	
+	//recherche de nourriture à l'extérieur de la fourmilière
+	public void foodResearch()
+	{
+		//déplacement aléatoire jusqu'a rencontre phéromone ou nourriture
+		//tuer animal
+		//rapporter nourriture
+	}
+	
+	@Override
+	public void update(int delta)
+	{
+		
+	}
+	
+	@Override
+	public Collection<TouchMarker> getTouchMarker() {
+		return new ArrayList<TouchMarker>(0);
+	}
+
+	@Override
+	public Collection<TouchHandle> getTouchHandle() {
+		return new ArrayList<TouchHandle>(0);
+	}	
+}
