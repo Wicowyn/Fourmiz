3c9e3b0f7606db554349295e01a7649b520cce2e
jcamenen <jcamenen@gmail.com>
2013-12-05 07:31:04 -0800

    Ajout des commentaires de la classe

--- src/fourmiz/abillity/Queen.java
+++ src/fourmiz/abillity/Queen.java
@@ -23,6 +23,7 @@ public class Queen extends Abillity{
 	public void update(int delta){
 		time+=delta;
 		
+		//si l'age de la reine à dépassé ou est égal à la date de ponte suivante 
 		if(time>=timeToLay){
 			time=0;
 			generateTimeToLay();
@@ -31,9 +32,12 @@ public class Queen extends Abillity{
 		}
 	}
 	
+	//ponte d'un oeuf
 	private void lay(){
+		//création d'une nouvelle entité de type oeuf
 		Entity egg=EntityFactory.createEntity(EntityName.Egg, getOwner().getEngine());
 		
+		//on lui donne la position de la reine au moment de la ponte
 		Vector2f position=getOwner().getPosition();
 		position.x+=(float) (Engine.SIZE_CASE*Math.cos(Math.toRadians(getOwner().getDirection()-180)));
 		position.y+=(float) (Engine.SIZE_CASE*Math.sin(Math.toRadians(getOwner().getDirection()-180)));
@@ -41,9 +45,11 @@ public class Queen extends Abillity{
 		egg.setPosition(position);
 		egg.setDirection(getOwner().getDirection());
 		
+		//on ajoute l'oeuf à la liste des entités
 		getOwner().getEngine().addEntityToBuff(egg);
 	}
 	
+	//génération du prochain temps de ponte
 	private void generateTimeToLay(){
 		timeToLay=3000+((int) Math.random()%3000)-1000;
 	}
